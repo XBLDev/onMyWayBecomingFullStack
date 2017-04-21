00c6f34c37c53e040405a23ecfa53bf02e415e74
XBLDev <u5260839@anu.edu.au>
2017-04-21 20:05:32 +1000

    updated JS entrance file, with page transition from page1 to page2 without animation.

--- Sample/index.android.js
+++ Sample/index.android.js
@@ -1,7 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
+
  */
 
 import React, { Component } from 'react';
@@ -9,25 +7,84 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Animated,
 } from 'react-native';
 
-export default class aSample extends Component {
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome2}>
-          CONTENT
+      <Text>{display}</Text>
+    );
+  }
+}
+
+export default class aSample extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {readText: true, firstPage: true};
+    this._onPressed = this._onPressed.bind(this);
+  }
+  _onPressed() {
+   
+    this.setState({ readText: !this.state.readText });
+    this.setState({ firstPage: !this.state.firstPage });
+  }
+  
+  render() {
+
+    
+	let pic = this.state.readText ? {
+      uri: 'https://d30y9cdsu7xlg0.cloudfront.net/png/1033-200.png'
+    }:{uri: 'https://icons.iconarchive.com/icons/rokey/smooth/128/apple-icon.png'};  
+	
+    let bodystyle = this.state.firstPage ? styles.instructions:styles.instructions2;
+    let headerstyle = this.state.firstPage ? styles.welcome2:styles.welcome4;
+    let header = this.state.firstPage ? 'CONTENT' : 'WE WILL ANSWER:';
+    let bodytext = this.state.firstPage ? 'Get Customers'+"\n"+'Interested by Telling'+"\n"+'a Great Story' : 'How can I use my own experience to help my business?'+'\n'+'Whats the personal approach to story telling'+'\n'+' whats the higher purpose approach?';
+    let author = this.state.firstPage ? 'Ryan Holiday, Author and Strategist' : '';
+    let timeAndActivity = this.state.firstPage ? '3min, 2 activities' : '';
+    
+    return (
+      <TouchableWithoutFeedback onPress={this._onPressed} style={styles.container}>
+      <View style={styles.container} >
+	    <Image source={pic} style={{width: 50, height: 50}} />
+        
+        <Text style={headerstyle}  >
+         {header}
         </Text>
-        <Text style={styles.instructions}>
-          Get Customers{'\n'}
-		  Interested by telling{'\n'}
-		  a story
+        <Text style={bodystyle}>
+        {bodytext}
         </Text>
-        <Text style={styles.instructions2}>
-          Ryan Holiday, Author and Strategist
+        <Text style={styles.welcome3}>
+          {author}
         </Text>
+		
+		
+        <Text style={styles.welcome3}>
+          {timeAndActivity}
+        </Text>
+        
+        <Blink text='Tap to continue' />
       </View>
+        </TouchableWithoutFeedback>
+     
     );
   }
 }
@@ -35,9 +92,18 @@ export default class aSample extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'center',//<-----justifyContent: how are children distributed?Available options are flex-start, center, flex-end, space-around, and space-between.
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+	//flexDirection: 'row'
+	
+  },
+  welcome4: {
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 30,
+    color: '#000000',
+    fontWeight: 'bold'
   },
   welcome: {
     fontSize: 20,
@@ -45,18 +111,35 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   welcome2: {
-    fontSize: 20,
+    fontSize: 15,
     textAlign: 'center',
-    margin: 10,
-	color: '#1E90FF',
+    margin: 30,
+	color: '#00ffab',
+    fontWeight: 'bold'
+  },
+  welcome3: {
+    fontSize: 11,
+    textAlign: 'center',
+    margin: 5,
+	color: '#D3D3D3',
+    //fontWeight: 'bold'
   },
   
   instructions: {
     textAlign: 'center',
     color: '#000000',
-    marginBottom: 5,
+    marginBottom: 10,
+    fontSize: 20,
+    fontWeight: 'bold'
   },
   instructions2: {
+    textAlign: 'center',
+    color: '#000000',
+    marginBottom: 5,
+    fontSize: 12,
+  },
+  
+  instructions3: {
     textAlign: 'center',
     color: '#D3D3D3',
     marginBottom: 5,
